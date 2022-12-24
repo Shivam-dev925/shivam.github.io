094325bb269563d7be16d21f13d8183ba5c4ef7b
shivam choudhary <chshivam815@gmail.com>
2022-12-24 21:05:05 +0530

    typescript modules added

---
+++ typeScriptPortfolio/myportfolio/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import photo from "./Assets/images/me.jpeg";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={photo} className="App-logo" alt="logo" />
+        <p className="text1">Shivam Choudhary</p>
+
+        <h6 className="text2">portfolio will be here very soon !</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
